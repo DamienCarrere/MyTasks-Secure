18a777c17867dcf52b270e9b10fd79e740dd6f10
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-09 20:26:07 +0200

    add require in index

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// require_once __DIR__ . "/app/Controller/AuthController.php";
+// require_once __DIR__ . "/app/Controller/ProfileController.php";
+// require_once __DIR__ . "/app/Controller/TaskController.php";
+
+require __DIR__ . "/config/Database.php";
