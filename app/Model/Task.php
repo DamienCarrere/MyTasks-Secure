ba75145e8bf97a0b8d5de687f4069ba93e4140af
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-09 22:39:44 +0200

    add getters for Task and User

--- app/Model/Task.php
+++ app/Model/Task.php
@@ -19,4 +19,28 @@ class Task
         $this->due_date = $due_date;
         $this->done = $done;
     }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+    public function getDueDate(): \DateTime
+    {
+        return $this->due_date;
+    }
+    public function getDone(): bool
+    {
+        return $this->done;
+    }
 }
