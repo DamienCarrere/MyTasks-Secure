66542ea9f142c929cb8573bb68e025c27e486952
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-09 22:55:04 +0200

    update create in UserDAO

--- app/DAO/UserDAO.php
+++ app/DAO/UserDAO.php
@@ -10,8 +10,10 @@ class UserDAO
         $this->pdo = $pdo;
     }
 
-    public function create(string $name,  string $email, string $password)
+    public function create(User $user)
     {
-        $query = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $query = "INSERT INTO users (name, firstname, email, password) VALUES (:name, :firstname, :email, :password)";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([":name" => $user->getName(), ":firstname" => $user->getFirstname(), ":email" => $user->getEmail(), ":password" => $user->getPassword()]);
     }
 }
