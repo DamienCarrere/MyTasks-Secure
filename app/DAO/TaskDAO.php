4cbd3ca9b9928dbd2220ec6443e31da5be133ab6
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-09 22:36:16 +0200

    push task waiting for patch task with getters

---
+++ app/DAO/TaskDAO.php
@@ -0,0 +1,19 @@
+<?php
+
+class TaskDAO
+{
+
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function create(Task $task)
+    {
+        $query = "INSERT INTO users (title, description, due_date, done) VALUES (:title, :description, :due_date, :done)";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([":title" => $task->getTitle(), ":description" => $task->getDescription(), ":due_date" => $task->getDueDate(), ":done" => $task->getDone()]);
+    }
+}
