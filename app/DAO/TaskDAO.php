9aa858f2744361255c6e533d87dd8a184af93384
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-09 23:31:05 +0200

    update & redo structure

--- app/DAO/TaskDAO.php
+++ app/DAO/TaskDAO.php
@@ -14,6 +14,13 @@ class TaskDAO
     {
         $query = "INSERT INTO users (title, description, due_date, done) VALUES (:title, :description, :due_date, :done)";
         $statement = $this->pdo->prepare($query);
-        $statement->execute([":title" => $task->getTitle(), ":description" => $task->getDescription(), ":due_date" => $task->getDueDate(), ":done" => $task->getDone()]);
+        $statement->execute(
+            [
+                ":title" => $task->getTitle(),
+                ":description" => $task->getDescription(),
+                ":due_date" => $task->getDueDate(),
+                ":done" => $task->getDone()
+            ]
+        );
     }
 }
