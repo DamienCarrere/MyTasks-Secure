a1d1b4ff04cd76ee9270374a0887dc83e417b1b3
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-09 23:49:41 +0200

    add basics methods for UserDAO

--- app/DAO/UserDAO.php
+++ app/DAO/UserDAO.php
@@ -14,6 +14,62 @@ class UserDAO
     {
         $query = "INSERT INTO users (name, firstname, email, password) VALUES (:name, :firstname, :email, :password)";
         $statement = $this->pdo->prepare($query);
-        $statement->execute([":name" => $user->getName(), ":firstname" => $user->getFirstname(), ":email" => $user->getEmail(), ":password" => $user->getPassword()]);
+        $statement->execute(
+            [
+                ":name" => $user->getName(),
+                ":firstname" => $user->getFirstname(),
+                ":email" => $user->getEmail(),
+                ":password" => $user->getPassword()
+            ]
+        );
+    }
+
+    public function findByEmail(User $user)
+    {
+        $query = "SELECT * FROM users WHERE email = :email LIMIT 1";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([":email" => $user->getEmail()]);
+    }
+
+    public function findById(User $user)
+    {
+        $query = "SELECT * FROM users WHERE id = :id LIMIT 1";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([":id" => $user->getId()]);
+    }
+
+    public function updateProfile(User $user)
+    {
+        $query = "UPDATE users SET name = :name, email = :email WHERE id = :id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":name" => $user->getName(),
+                ":id" => $user->getId(),
+                ":email" => $user->getEmail()
+            ]
+        );
+    }
+    public function updatePassword(User $user)
+    {
+        $query = "UPDATE users SET password = :password WHERE id = :id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":password" => $user->getPassword(),
+                ":id" => $user->getId()
+            ]
+        );
+    }
+
+    public function delete(User $user)
+    {
+        $query = "DELETE FROM users WHERE id = :id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":id" => $user->getId()
+            ]
+        );
     }
 }
