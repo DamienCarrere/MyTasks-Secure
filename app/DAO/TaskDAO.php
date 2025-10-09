8c4b23e7731cbd7edcb7b7b86b15c0ab2d7cc611
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-10 00:07:41 +0200

    add methods to TaskDAO

--- app/DAO/TaskDAO.php
+++ app/DAO/TaskDAO.php
@@ -23,4 +23,63 @@ class TaskDAO
             ]
         );
     }
+
+    public function findAllByUser(Task $task)
+    {
+        $query = "SELECT * FROM tasks WHERE user_id = :user_id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":user_id" => $task->getUserId()
+            ]
+        );
+    }
+
+    public function findById(Task $task)
+    {
+        $query = "SELECT * FROM tasks WHERE id = :id LIMIT 1";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":id" => $task->getId()
+            ]
+        );
+    }
+
+    public function update(Task $task)
+    {
+        $query = "UPDATE tasks SET title = :title, description = :description, due_date = :due_date WHERE id = :id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":title" => $task->getTitle(),
+                ":description" => $task->getDescription(),
+                ":due_date" => $task->getDueDate(),
+                ":id" => $task->getDone()
+            ]
+        );
+    }
+
+    public function delete(Task $task)
+    {
+        $query = "DELETE FROM tasks WHERE id = :id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":id" => $task->getId()
+            ]
+        );
+    }
+
+    public function toggleDone(Task $task)
+    {
+        $query = "UPDATE tasks SET done = :done WHERE id = :id";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(
+            [
+                ":done" => $task->getDone(),
+                ":id" => $task->getId()
+            ]
+        );
+    }
 }
