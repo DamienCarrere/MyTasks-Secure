f877e894f9f28c4eba0b21b486bd1cf15268a9e9
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-10 12:26:49 +0200

    add jump lines

--- app/View/auth/register.php
+++ app/View/auth/register.php
@@ -18,11 +18,11 @@
     <?php endif; ?>
 
     <form method="POST">
-        <input type="text" name="name" placeholder="Nom" value="<?= htmlspecialchars($name ?? "", ENT_QUOTES, "UTF-8") ?>">
-        <input type="text" name="firstname" placeholder="Prénom" value="<?= htmlspecialchars($firstname ?? "", ENT_QUOTES, "UTF-8") ?? "" ?>">
-        <input type="email" name="email" placeholder="Email" value="<?= htmlspecialchars($email ?? "", ENT_QUOTES, "UTF-8") ?>">
-        <input type="password" name="password" placeholder="Mot de passe">
-        <input type="password" name="password_confirmation" placeholder="Confirmer le mot de passe">
+        <input type="text" name="name" placeholder="Nom" value="<?= htmlspecialchars($name ?? "", ENT_QUOTES, "UTF-8") ?>"><br>
+        <input type="text" name="firstname" placeholder="Prénom" value="<?= htmlspecialchars($firstname ?? "", ENT_QUOTES, "UTF-8") ?? "" ?>"><br>
+        <input type="email" name="email" placeholder="Email" value="<?= htmlspecialchars($email ?? "", ENT_QUOTES, "UTF-8") ?>"><br>
+        <input type="password" name="password" placeholder="Mot de passe"><br>
+        <input type="password" name="password_confirmation" placeholder="Confirmer le mot de passe"><br>
         <button type="submit">S'inscrire</button>
     </form>
 
