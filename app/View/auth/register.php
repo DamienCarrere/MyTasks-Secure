399368741cae2cb8bbb06da4a6c2c47d0d5b5b10
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-10 11:55:01 +0200

    add AuthController

---
+++ app/View/auth/register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <h3>REGISTER</h3>
+    <?php if (!empty($errors)): ?>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?= htmlspecialchars($error, ENT_QUOTES, "UTF-8") ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
+    <form method="POST">
+        <input type="text" name="name" placeholder="Nom" value="<?= htmlspecialchars($name ?? "", ENT_QUOTES, "UTF-8") ?>">
+        <input type="text" name="firstname" placeholder="Prénom" value="<?= htmlspecialchars($firstname ?? "", ENT_QUOTES, "UTF-8") ?? "" ?>">
+        <input type="email" name="email" placeholder="Email" value="<?= htmlspecialchars($email ?? "", ENT_QUOTES, "UTF-8") ?>">
+        <input type="password" name="password" placeholder="Mot de passe">
+        <input type="password" name="password_confirmation" placeholder="Confirmer le mot de passe">
+        <button type="submit">S'inscrire</button>
+    </form>
+
+</body>
+
+</html>
