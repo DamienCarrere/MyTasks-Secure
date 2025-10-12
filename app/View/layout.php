8ba2dfa025be0d18c2484409f4647d2afb2778ec
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-12 19:20:35 +0200

    modify css

--- app/View/layout.php
+++ app/View/layout.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyTasks Secure</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/public/style.css">
 </head>
 
-<body>
+<body class="bg-dark text-light">
     <header>
         <h2><?= $header ?></h2>
     </header>
