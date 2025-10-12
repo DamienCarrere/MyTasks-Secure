eabf5e4ef5d96e5178a78cd4d1216d4216d0b825
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-12 15:33:31 +0200

    change TaskDAO

--- app/Model/Task.php
+++ app/Model/Task.php
@@ -7,7 +7,7 @@ class Task
     protected int $user_id;
     protected string $title;
     protected string $description;
-    protected \DateTime $due_date;
+    protected DateTime $due_date;
     protected bool $done;
 
     public function __construct(?int $id, int $user_id, string $title, string $description, \DateTime $due_date, bool $done)
@@ -18,29 +18,5 @@ class Task
         $this->description = $description;
         $this->due_date = $due_date;
         $this->done = $done;
-    }
-    public function getId(): int
-    {
-        return $this->id;
-    }
-    public function getUserId(): int
-    {
-        return $this->user_id;
-    }
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-    public function getDueDate(): \DateTime
-    {
-        return $this->due_date;
-    }
-    public function getDone(): bool
-    {
-        return $this->done;
     }
 }
