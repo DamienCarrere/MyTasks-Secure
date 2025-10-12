db0616c8800a7022f1d194bee1d54171eb3c6a96
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-12 17:53:09 +0200

    update create & edit

--- app/View/task/create.php
+++ app/View/task/create.php
@@ -15,9 +15,21 @@
         <label>Description</label>
         <input type="text" name="description">
         <label>Date d'échéance</label>
-        <input type="date" name="due_date">
+        <input type="datetime-local" name="due_date">
         <button type="submit">Créer la tâche</button>
     </form>
+
+    <?php if (!empty($is_done)): ?>
+        <p style="color:green;"><?= htmlspecialchars($is_done) ?></p>
+    <?php endif; ?>
+
+    <?php if (!empty($errors)): ?>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?= htmlspecialchars($error, ENT_QUOTES, "UTF-8") ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </body>
 
 </html>
---
+++ app/View/task/edit.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit</title>
+</head>
+
+<body>
+    <h3>EDIT</h3>
+
+    <h4>Tâche: <?= htmlspecialchars($task["title"]) ?></h4>
+    <form method="POST">
+        <label>Titre</label>
+        <input type="text" name="title" value="<?= htmlspecialchars($task["title"]) ?>>
+        <label>Description</label>
+        <textarea type=" text" name="description"><?= htmlspecialchars($task["description"]) ?></textarea>
+        <label>Date d'échéance</label>
+        <input type="datetime-local" name="due_date" value="<?= htmlspecialchars($task["due_date"]) ?>">
+        <button type="submit">Modifier la tâche</button>
+    </form>
+    <?php if (!empty($is_done)): ?>
+        <p style="color:green;"><?= htmlspecialchars($is_done) ?></p>
+    <?php endif; ?>
+
+    <?php if (!empty($errors)): ?>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?= htmlspecialchars($error, ENT_QUOTES, "UTF-8") ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <br>
+    <a href="index.php?controller=task&action=index">Mes Tâches</a>
+</body>
+
+</html>
