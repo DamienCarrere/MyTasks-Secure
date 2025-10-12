46cd2c43631a5216e86493ced06e752580cf5003
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-10-12 19:11:03 +0200

    better html button

--- app/View/task/list.php
+++ app/View/task/list.php
@@ -24,11 +24,10 @@ $header = "List";
                     <td><?= htmlspecialchars($task["description"]) ?></td>
                     <td><?= htmlspecialchars($task["due_date"]) ?></td>
                     <td>
-                        <?= $task["done"] ? "✔️" : "❌" ?>
                         <form method="POST" action="index.php?controller=task&action=toggleDone" style="display:inline;">
                             <input type="hidden" name="id" value="<?= $task['id'] ?>">
                             <input type="hidden" name="done" value="<?= $task['done'] ? 1 : 0 ?>">
-                            <button type="submit">Changer status</button>
+                            <button type="submit"><?= $task["done"] ? "✔️" : "❌" ?></button>
                         </form>
                     </td>
                     <td>
